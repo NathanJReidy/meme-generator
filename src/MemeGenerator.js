f2c0ebaecc4fce7378f81bae5eb6d46100b694c9
Nathan Reidy <nathanjreidy@gmail.com>
2020-08-05 09:22:18 +1000

    Connected components

---
+++ src/MemeGenerator.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Header from './Header';
+
+class MemeGenerator extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+
+        }
+    }
+
+    handleDidMount() {
+        .fetch("")
+        .then()
+    }
+
+    render() {
+        return (
+            <Header />
+        )
+    }
+}
+
+export default MemeGenerator
