01e5c1b6484e5d2605c2a3d958868564248fe1bc
Nathan Reidy <nathanjreidy@gmail.com>
2020-08-05 14:25:06 +1000

    Updates

---
+++ precache-manifest.d9e52d5cd913c9899a84ce5ec9eb5136.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb34b0ddd3eabef0894935e965bd8482",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "4bb649145dbcfd44a602",
+    "url": "/meme-generator/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4831f0b1bfc17b3266e8",
+    "url": "/meme-generator/static/js/2.02143386.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meme-generator/static/js/2.02143386.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bb649145dbcfd44a602",
+    "url": "/meme-generator/static/js/main.3ef3cd16.chunk.js"
+  },
+  {
+    "revision": "163fc7ea317d65653242",
+    "url": "/meme-generator/static/js/runtime-main.66d5941a.js"
+  }
+]);
